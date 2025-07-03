b77c9746de02761c1134cfda631471b6d9dc4c32
wei-zhe <weizheweng@google.com>
2025-07-03 23:34:04 +0800

    Add @types/node dependency and update TypeScript configuration; enhance Vite config with path aliasing and server settings.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,12 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
 import checker from 'vite-plugin-checker'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -21,4 +27,14 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    port: 3000,
+    open: true,
+    host: '0.0.0.0',
+  },
+  resolve: {
+    alias: {
+      '@ui': path.resolve(__dirname, './src/ui/index.ts'),
+    },
+  },
 })
