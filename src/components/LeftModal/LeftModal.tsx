57770d954d8d135b5b83052d4ab3c1bf54c6773e
zackweng <weizheweng@gmail.com>
2025-07-07 01:26:14 +0800

    Refactor LeftModal component to utilize Redux state for dynamic class information and enhance styling with background color and border radius.

--- src/components/LeftModal/LeftModal.tsx
+++ src/components/LeftModal/LeftModal.tsx
@@ -1,29 +1,27 @@
 import { VStack, HStack, Text } from '@ui'
 import QRCode from 'react-qr-code'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import styled from 'styled-components'
 
 import { closeLeftModal } from '../../store/modalSlice'
-import { Close, Left } from '../icons/Icons'
+import { colors } from '../../styles/colors'
+import { CloseIcon } from '../CloseIcon'
+import { Left } from '../icons/Icons'
 
 import { CopyField } from './CopyField'
 
-const CloseIcon = styled.button`
-  position: absolute;
-  top: 8px;
-  right: 8px;
-  background: none;
-  cursor: pointer;
-  border: none;
-`
+import type { RootState } from 'src/store'
 
 const ModalContent = styled.div`
   position: relative;
   min-height: 300px;
   padding: 24px 32px;
+  background-color: ${colors.grayLight};
+  border-radius: 24px;
 `
 
 export const LeftModal = () => {
+  const { className, id, link } = useSelector((state: RootState) => state.student)
   const dispatch = useDispatch()
 
   const handleClose = () => {
@@ -32,18 +30,16 @@ export const LeftModal = () => {
 
   return (
     <ModalContent>
-      <CloseIcon onClick={handleClose} aria-label="Close">
-        <Close width={36} height={36} />
-      </CloseIcon>
+      <CloseIcon onClick={handleClose} />
       <VStack gap="16px">
         <HStack gap="8px" align="center" style={{ cursor: 'pointer' }}>
           <Left width={12} height={12} />
           <Text fontSize="12px" fontWeight={700}>Back to Class List</Text>
         </HStack>
-        <Text fontSize="24px" fontWeight={700}>Join 302 Science</Text>
+        <Text fontSize="24px" fontWeight={700}>{`Join ${className}`}</Text>
         <HStack gap="32px" align="center">
-          <CopyField label="ID: X58E9647" text="X58E9647" />
-          <CopyField label="Link" text="https://www.classswift.viewsonic.io/" />
+          <CopyField label="ID: X58E9647" text={`ID: ${id}`} />
+          <CopyField label="Link" text={link ?? ''} />
         </HStack>
         <VStack gap="12px" align="center">
           <QRCode value="https://www.classswift.viewsonic.io/" style={{ width: '80%', height: 'auto' }} title="Class Swift" />
