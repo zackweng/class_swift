a47ec6a4bc13696c558862e9f2c3a7568d49fcc0
zackweng <weizheweng@gmail.com>
2025-07-07 01:25:19 +0800

    Add LoadingIcon SVG and integrate into Icons component for loading state representation.

--- src/components/icons/Icons.tsx
+++ src/components/icons/Icons.tsx
@@ -3,6 +3,7 @@ import CloseIcon from '@assets/close.svg?react'
 import DotsVerticalIcon from '@assets/dots-vertical.svg?react'
 import DuplicateIcon from '@assets/duplicate.svg?react'
 import LeftIcon from '@assets/left.svg?react'
+import LoadingIcon from '@assets/loading.svg?react'
 import PersonIcon from '@assets/person.svg?react'
 
 export const Check = CheckIcon
@@ -10,4 +11,5 @@ export const Close = CloseIcon
 export const DotsVertical = DotsVerticalIcon
 export const Duplicate = DuplicateIcon
 export const Left = LeftIcon
+export const Loading = LoadingIcon
 export const Person = PersonIcon
